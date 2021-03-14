546cb0d593341acdb31d965d0247cb4e20cbc048
endon <en.nobu3.9@gmail.com>
2021-03-14 16:29:11 +0900

    change:batファイルの完了メッセージ修正

--- databases/cleanup_kashilog_data.bat
+++ databases/cleanup_kashilog_data.bat
@@ -6,6 +6,6 @@ docker cp ./MSSql/initialize/CleanUpKashilog.sql kashilogdb:/tmp/cleanupkashilog
 echo Cleanup data for kashilogdb from sql file...
 docker exec -it kashilogdb /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P h0geFuga -i /tmp/cleanupkashilog.sql
 
-echo Cleanup data task for kashilogdb is Completed!
+echo Cleanup data for kashilogdb is Completed!
 
 pause
--- databases/create_kashilog_data.bat
+++ databases/create_kashilog_data.bat
@@ -6,6 +6,6 @@ docker cp ./MSSql/initialize/Insert_KashilogData.sql kashilogdb:/tmp/insert_kash
 echo Create data for kashilogdb from sql file...
 docker exec -it kashilogdb /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P h0geFuga -i /tmp/insert_kashilogdata.sql
 
-echo Create data task for kashilogdb is Completed!
+echo Create data for kashilogdb is Completed!
 
 pause
